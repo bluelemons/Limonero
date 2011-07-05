31aa386db28e8301fb152963262ff0c15cbec442
El_Hoy <eloyesp@gmail.com>
2011-07-04 21:32:52 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Limonero::Application.config.secret_token = '5389775d0b540e7c09cfaee3f31d7d086dd8a10e258ab6edc724a68dab3410e8e33921df5cd254a70ff1652bd2aca110691693b890eaf21fdf76996af5a8ca6c'
