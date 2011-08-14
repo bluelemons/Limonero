2edcc6c35e0ef8a5fdcfd19930bb17dc335bd894
olvap <olvap@olvap-Calpella.(none)>
2011-08-13 22:28:43 -0300

    agrego unos metodos para hacer mas simples las vistas

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,3 +1,24 @@
 class Lot < ActiveRecord::Base
   has_many :avaluos
+
+  def titular
+    payment_actual.try(:contratos).try(:first).try(:person).try(:name)
+  end
+
+  def estado
+    if titular
+      "Vendido"
+    else
+      "Sin vender"
+    end
+  end
+
+  def pagable?
+    (titular)
+  end
+
+  def payment_actual
+    try(:avaluos).try(:last).try(:payments).try(:last)
+  end
+
 end
