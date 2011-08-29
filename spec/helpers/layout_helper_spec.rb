630f813f34fc22b0c8a249e19b7db303138e6f9c
Eloy Espinaco <eloyesp@gmail.com>
2011-08-28 22:07:01 -0300

    CLEAN: specs de helpers que no se usan

--- spec/helpers/layout_helper_spec.rb
+++ spec/helpers/layout_helper_spec.rb
@@ -3,13 +3,15 @@ require 'spec_helper'
 # Specs in this file have access to a helper object that includes
 # the LotesHelper. For example:
 #
-# describe LotesHelper do
+# describe LayoutHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       helper.concat_strings("this","that").should == "this that"
 #     end
 #   end
 # end
-describe LotesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe LayoutHelper do
+  describe "#error_message(model)" do
+    pending "add examples to this #{__FILE__}"
+  end
 end
