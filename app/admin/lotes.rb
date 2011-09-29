450d0c538ad712e279226aa0460110654735b5ed
Olvap <oldani.pablo@gmail.com>
2011-09-29 12:14:18 -0300

    ADD: agrego paginas mas cortas

--- app/admin/lotes.rb
+++ app/admin/lotes.rb
@@ -3,4 +3,13 @@ ActiveAdmin.register Lote do
     render "/lotes/lote"
   end
   
+  controller do
+      # This code is evaluated within the controller class
+
+      def index
+        @search = Lote.search(params[:search])
+        @lotes = @search.page(params[:page])
+        index!
+      end
+    end
 end
